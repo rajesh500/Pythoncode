536cfa549fe06d71c08772c7d2bbe02f9302f6c5
Rajesh Jakkula <rajeshjakkula@Rajeshs-MBP.home>
2020-06-30 20:48:22 -0700

    List of int and string are reversed

---
+++ data_structures/list_revsr.py
@@ -0,0 +1,12 @@
+my_list=[2,3,4,5,6,7,8,9]
+my_list1=['A', 'B','C','D']
+my_list.reverse()
+my_list1.reverse()
+print(my_list)
+print(my_list1)
+#####
+# simply text will be reversed
+s="This is the example of loop"
+t=s.split()
+t.reverse()
+print(t)
