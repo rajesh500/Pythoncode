435d1626da08f57225d904c9e153d1b90bdd8d42
Rajesh Jakkula <rajeshjakkula@Rajeshs-MBP.home>
2020-06-30 18:57:50 -0700

    reverse the text and and reverse the word

--- txt_revrse.py
+++ txt_revrse.py
@@ -11,3 +11,9 @@ print(v)
 # 2)
 for i in reversed(t):
     print(i, end=' ')
+
+# 3)
+a="This is the example of loop and standard"
+b=a.split()
+for i in reversed(b):
+  print(i[::-1])
--- wrd_revrse_txt.py
+++ wrd_revrse_txt.py
@@ -1,10 +1,8 @@
+# word reverse in text
+
 n="This is while loop examples"
 m=n.split()
-# print(len(m))
 for i in range(len(m)):
-    print(m[i][::-1], end=' ')
-
-
-
+     print(m[i][::-1], end=' ')
 
 
